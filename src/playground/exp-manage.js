64ede78f8e01b960d06d9acba648be482e930023
ErickObadha <erickobadha@gmail.com>
2020-03-13 14:37:47 +0300

    redux

--- src/playground/exp-manage.js
+++ src/playground/exp-manage.js
@@ -1,1 +1,58 @@
-console.log('Redux is fantastic');
+import { createStore, combineReducers } from 'redux';
+import uuid from 'uuid';
+
+//ADD_EXPENSES
+const addExpense = (expense) => {
+	return {
+		type: 'ADD_EXPENSE',
+		expense: {
+			id: uuid(),
+			description: expense.description ? expense.description : '',
+			note: expense.note ? expense.note: '',
+			amount: expense.amount ? expense.amount: 0,
+			createdAt: expense.createdAt ? expense.createdAt: 0
+		}
+	}
+}
+
+//Expenses Reducer
+const expensesReducerDefaultState = [];
+const expensesReducer = (state = expensesReducerDefaultState, action) => {
+	switch (action.type) {
+		case 'ADD_EXPENSE':
+		return state.concat(action.expense)
+		default:
+		return state;
+	}
+}
+
+//filters reducer
+
+const filtersReducerDefaultState = {
+	text: '',
+	sortBy: 'date',
+	startDate: undefined,
+	endDate: undefined
+}
+const filtersReducer = (state = filtersReducerDefaultState, action) => {
+	switch (action.type) {
+		default:
+		return state;
+	}
+}
+
+// Store creation
+const store = createStore(
+	combineReducers({
+		expenses: expensesReducer,
+		filters: filtersReducer
+	})
+	);
+
+store.subscribe(()=>{
+	console.log(store.getState());
+})
+const expenseOne = store.dispatch(addExpense({description: 'Vacation', amount: 500}));
+
+const expenseTwo = store.dispatch(addExpense({ description: 'Coffe', amount: 600 })
+	);
